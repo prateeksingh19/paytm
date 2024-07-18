a4193674546bcb98e830b05a6b67e69d7bf6e401
Prateek Singh <prateekvns99@gmail.com>
2024-07-18 16:26:11 +0530

    testing 2

--- components/button.tsx
+++ components/button.tsx
@@ -10,7 +10,10 @@ interface ButtonProps {
 export const Button = ({ onClick, children }: ButtonProps) => {
   return (
     <button
-      onClick={onClick}
+      onClick={onClick}     
+
+
+      
       className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
     >
       {children}

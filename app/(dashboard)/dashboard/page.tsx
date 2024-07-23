1bfa469cf34dbbbd47e3421d256052014927a8d9
Prateek Singh <prateekvns99@gmail.com>
2024-07-23 20:29:46 +0530

    test

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,11 @@
+import { Card } from "@/components/card";
+
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <div>
+      <div>Dashboard</div>
+      <Card title={"OnRamp Transactions"} />
+      <Card title={"P2P Transactions"} />
+    </div>
+  );
 }
